592c26be6f94038c35586b030ab3b55763408e32
Eric Gitonga <gitonga@gmail.com>
2022-07-12 22:31:32 +0300

    improve reporting of provincial data

--- cipe.py
+++ cipe.py
@@ -20,14 +20,21 @@ kenya["Number of MSMEs"] = [156, 264, 152, 148, 121, 145, 149, 145]
 kenya["MSMEs Online"] = [55, 183, 10, 74, 62, 51, 81, 134]
 kenya["Percentage of MSMEs Online"] = round((100 * kenya["MSMEs Online"] / kenya["Number of MSMEs"]), 2)
 
-with streamlit_analytics.track():
+with streamlit_analytics.track():#unsafe_password="youandme"):
     province = st.sidebar.selectbox("Select a province", kenya["Province"], index=0)
 
     m = leafmap.Map(center=[-0.02, 37.91], zoom=5, height=600, widescreen=False)
 
     if st.checkbox("Filter by province"):
+        index = list(kenya["Province"]).index(province)
+        prov = kenya.loc[index, "Province"]
+        number_of_smes = kenya.loc[index, "Number of MSMEs"]
+        smes_online = kenya.loc[index, "MSMEs Online"]
+        percentage_online = kenya.loc[index, "Percentage of MSMEs Online"]
+
+        st.write(f"In {prov} province, {number_of_smes} MSMEs were interviewed, out of which {smes_online} or {percentage_online}% are online.")
+
         m.add_gdf(kenya[kenya["Province"] == province], layer_name=province)
-        st.write(kenya[kenya["Province"] == province][["Province", "Number of MSMEs", "MSMEs Online", "Percentage of MSMEs Online"]])
     else:
         m.add_gdf(kenya, layer_name="Provinces")
     m.to_streamlit(height=500)
