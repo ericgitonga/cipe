493394212cf4b517d998c20bc92acd89dd05f867
Eric Gitonga <gitonga@gmail.com>
2022-07-13 18:35:15 +0300

    removed streamlit-analytics which causes code breakage with latest streamlit version

--- cipe.py
+++ cipe.py
@@ -4,7 +4,6 @@ import geopandas as gpd
 import json
 import leafmap.kepler as leafmap
 import streamlit as st
-import streamlit_analytics
 
 st.markdown("#### Micro, Small & Medium Enterprises Digital Economy Survey Report")
 
@@ -20,22 +19,20 @@ kenya["Number of MSMEs"] = [156, 264, 152, 148, 121, 145, 149, 145]
 kenya["MSMEs Online"] = [55, 183, 10, 74, 62, 51, 81, 134]
 kenya["Percentage of MSMEs Online"] = round((100 * kenya["MSMEs Online"] / kenya["Number of MSMEs"]), 2)
 
-with streamlit_analytics.track():#unsafe_password="youandme"):
-    province = st.sidebar.selectbox("Select a province", kenya["Province"], index=0)
+province = st.sidebar.selectbox("Select a province", kenya["Province"], index=0)
 
-    m = leafmap.Map(center=[-0.02, 37.91], zoom=5, height=600, widescreen=False)
+m = leafmap.Map(center=[-0.02, 37.91], zoom=5, height=600, widescreen=False)
 
-    if st.checkbox("Filter by province"):
-        index = list(kenya["Province"]).index(province)
-        prov = kenya.loc[index, "Province"]
-        number_of_smes = kenya.loc[index, "Number of MSMEs"]
-        smes_online = kenya.loc[index, "MSMEs Online"]
-        percentage_online = kenya.loc[index, "Percentage of MSMEs Online"]
+if st.checkbox("Filter by province"):
+    index = list(kenya["Province"]).index(province)
+    prov = kenya.loc[index, "Province"]
+    number_of_smes = kenya.loc[index, "Number of MSMEs"]
+    smes_online = kenya.loc[index, "MSMEs Online"]
+    percentage_online = kenya.loc[index, "Percentage of MSMEs Online"]
 
-        st.write(f"In {prov} province, {number_of_smes} MSMEs were interviewed, out of which {smes_online} or {percentage_online}% are online.")
-
-        m.add_gdf(kenya[kenya["Province"] == province], layer_name=province)
-    else:
-        m.add_gdf(kenya, layer_name="Provinces")
-    m.to_streamlit(height=500)
+    st.write(f"In {prov} province, {number_of_smes} MSMEs were interviewed, out of which {smes_online} or {percentage_online}% are online.")
+    m.add_gdf(kenya[kenya["Province"] == province], layer_name=province)
+else:
+    m.add_gdf(kenya, layer_name="Provinces")
+m.to_streamlit(height=500)
 
