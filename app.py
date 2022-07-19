3001627070bad075efa53bdf9f94f7292ce0264f
Eric Gitonga <gitonga@gmail.com>
2022-07-19 17:48:34 +0300

    fix menu option display bug

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from pages import msmes_online
+from apps import home, msmes_online
 
 st.set_page_config(page_title="MSME Digital Economy Survey Report", layout="wide")
 
-pages = {
-         "home": {"title": "Home", "icon": "house"},
-         "01_msmes_online": {"title": "Number of MSMEs Online", "icon": "map"},
-}
+apps = [
+         {"func": home.app, "title": "Home", "icon": "house"},
+         {"func": msmes_online.app, "title": "Number of MSMEs Online", "icon": "geo-alt-fill"},
+]
 
-titles = [page["title"] for page in pages.values()]
-icons = [page["icon"] for page in pages.values()]
+titles = [app["title"] for app in apps]
+titles_lower = [title.lower() for title in titles]
+icons = [app["icon"] for app in apps]
 
-st.markdown("## MSME Digital Economy Survey Report")
-st.write("Visualizations for the report")
+params = st.experimental_get_query_params()
+
+if "page" in params:
+    default_index = int(titles_lower.index(params["page"][0].lower()))
+else:
+    default_index = 0
+
+with st.sidebar:
+    selected = option_menu(
+        "Main Menu",
+        options=titles,
+        icons=icons,
+        menu_icon="cast",
+        default_index=default_index,
+    )
+    
+for app in apps:
+    if app["title"] == selected:
+        app["func"]()
+        break
 
---
+++ apps/home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def app():
+    st.title("Home")
+
+    st.markdown("### Visualizing MSME Digital Report Data")
