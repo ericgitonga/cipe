6f85cabc42b8749e0afc18c0dbeaae20db52e643
Eric Gitonga <gitonga@gmail.com>
2022-07-20 01:25:55 +0300

    hide header/footer/menu

--- apps/home.py
+++ apps/home.py
@@ -4,3 +4,13 @@ def app():
     st.title("Home")
 
     st.markdown("### Visualizing MSME Digital Report Data")
+    
+    hide_st_style = """
+                    <style>
+                    #MainMenu {visibility: hidden;}
+                    footer {visibility: hidden;}
+                    header {visibility: hidden;}
+                    </style>
+                    """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+
